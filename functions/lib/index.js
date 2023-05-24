95b30b4f22b263635308843c9354f4696ed7ef2a
Kevin Westropp <mail@kevinpatrickwestropp.com>
2023-05-24 11:03:03 -0600

    Changes: - updates to functions

---
+++ functions/lib/index.js
@@ -0,0 +1,15 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const functions = require("firebase-functions");
+const admin = require("firebase-admin");
+admin.initializeApp();
+// // Start writing Firebase Functions
+// // https://firebase.google.com/docs/functions/typescript
+//
+exports.helloWorld = functions.https.onRequest((request, response) => {
+    response.send("Hello from Firebase!");
+});
+exports.helloWorldCallable = functions.https.onCall((data, context) => {
+    return "Hello from Firebase!";
+});
+//# sourceMappingURL=index.js.map
